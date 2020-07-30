7854d34b0c507d7968d546a1c7f197c55e7c6402
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-07-30 19:01:19 +0800

    s 3

--- graph/train_my.py
+++ graph/train_my.py
@@ -6,6 +6,267 @@ from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from dataset import load
 
 
+
+class GcnInfomax(nn.Module):
+    def __init__(self, hidden_dim, num_gc_layers, alpha=0.5, beta=1., gamma=.1):
+        super(GcnInfomax, self).__init__()
+
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+        self.prior = args.prior
+
+        self.embedding_dim = mi_units = hidden_dim * num_gc_layers
+        self.encoder = Encoder(dataset_num_features, hidden_dim, num_gc_layers)
+        self.decoder = Decoder(hidden_dim, hidden_dim, dataset_num_features)
+
+        self.local_d = FF(self.embedding_dim)
+        self.global_d = FF(self.embedding_dim)
+
+
+        self.init_emb()
+
+    def init_emb(self):
+        initrange = -1.5 / self.embedding_dim
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                torch.nn.init.xavier_uniform_(m.weight.data)
+                if m.bias is not None:
+                    m.bias.data.fill_(0.0)
+
+
+    def forward(self, x, edge_index, batch, num_graphs):
+
+        # batch_size = data.num_graphs
+
+        node_mu, node_logvar, class_mu, class_logvar = self.encoder(x, edge_index, batch)
+
+
+        '''n_digits = 4
+        #print('before ', node_mu)
+        node_mu = (node_mu * 10**n_digits).round() / (10**n_digits)
+        node_logvar = (node_logvar * 10**n_digits).round() / (10**n_digits)
+        class_mu = (class_mu * 10**n_digits).round() / (10**n_digits)
+        class_logvar = (class_logvar * 10**n_digits).round() / (10**n_digits)'''
+
+
+        #print('direct out ', node_mu[0,:5], node_logvar[0,:5], class_mu[0,:5], class_logvar[0,:5], batch)
+
+
+        grouped_mu, grouped_logvar = accumulate_group_evidence(
+            class_mu.data, class_logvar.data, batch, True
+        )
+
+        #print('grouped ', grouped_mu[0,:5], grouped_logvar[0,:5])
+
+
+        #print('after ', node_mu)
+
+
+        # kl-divergence error for style latent space
+        node_kl_divergence_loss = torch.mean(
+            - 0.5 * torch.sum(1 + node_logvar - node_mu.pow(2) - node_logvar.exp())
+        )
+        #print('node kl unwei ', node_kl_divergence_loss, node_logvar, node_mu)
+
+
+        node_kl_divergence_loss = 0.0000001*node_kl_divergence_loss *num_graphs
+        #print('node kl wei ', node_kl_divergence_loss)
+
+
+        # kl-divergence error for class latent space
+        class_kl_divergence_loss = torch.mean(
+            - 0.5 * torch.sum(1 + grouped_logvar - grouped_mu.pow(2) - grouped_logvar.exp())
+        )
+        #print('class kl unwei ', class_kl_divergence_loss)
+        class_kl_divergence_loss = 0.0000001* class_kl_divergence_loss * num_graphs
+        #print('class kl wei ', class_kl_divergence_loss)
+
+
+        # reconstruct samples
+        """
+        sampling from group mu and logvar for each graph in mini-batch differently makes
+        the decoder consider class latent embeddings as random noise and ignore them 
+        """
+        node_latent_embeddings = reparameterize(training=True, mu=node_mu, logvar=node_logvar)
+        class_latent_embeddings = group_wise_reparameterize(
+            training=True, mu=grouped_mu, logvar=grouped_logvar, labels_batch=batch, cuda=True
+        )
+
+
+        reconstructed_node = self.decoder(node_latent_embeddings, class_latent_embeddings, edge_index)
+
+        #reconstruction_error =  mse_loss(reconstructed_node, x) * num_graphs
+        reconstruction_error = 0.01* self.recon_loss(reconstructed_node, edge_index, batch) * num_graphs
+
+
+        #print(reconstruction_error.item(), class_kl_divergence_loss.item(), node_kl_divergence_loss.item())
+
+        '''n_digits = 9
+        #print('before ', node_mu)
+        reconstruction_error = (reconstruction_error * 10**n_digits).round() / (10**n_digits)
+        class_kl_divergence_loss = (class_kl_divergence_loss * 10**n_digits).round() / (10**n_digits)
+        node_kl_divergence_loss = (node_kl_divergence_loss * 10**n_digits).round() / (10**n_digits)'''
+
+        class_kl_divergence_loss.backward(retain_graph=True)
+        node_kl_divergence_loss.backward(retain_graph=True)
+        reconstruction_error.backward()
+
+
+        return  reconstruction_error.item(), class_kl_divergence_loss.item() , node_kl_divergence_loss.item()
+
+
+    def edge_recon(self, z, edge_index, sigmoid=True):
+        r"""Decodes the latent variables :obj:`z` into edge probabilities for
+        the given node-pairs :obj:`edge_index`.
+
+        Args:
+            z (Tensor): The latent space :math:`\mathbf{Z}`.
+            sigmoid (bool, optional): If set to :obj:`False`, does not apply
+                the logistic sigmoid function to the output.
+                (default: :obj:`True`)
+        """
+        value = (z[edge_index[0]] * z[edge_index[1]]).sum(dim=1)
+        return torch.sigmoid(value) if sigmoid else value
+
+    def recon_loss(self, z, edge_index, batch):
+
+        EPS = 1e-15
+        MAX_LOGSTD = 10
+        r"""Given latent variables :obj:`z`, computes the binary cross
+        entropy loss for positive edges :obj:`pos_edge_index` and negative
+        sampled edges.
+  
+        Args:
+            z (Tensor): The latent space :math:`\mathbf{Z}`.
+            pos_edge_index (LongTensor): The positive edges to train against.
+        """
+
+        #reco = self.edge_recon(z, edge_index)
+
+        #print('edge recon try ', reco.size(), edge_index.size(), reco [:10], edge_index[0][:10], edge_index[1][:10])
+
+
+        #recon_adj = self.edge_recon(z, edge_index)
+
+        a, idx_tensor = to_dense_batch(z, batch)
+        a_t = a.permute(0, 2, 1)
+
+        #print('batch size', a.size(), a_t.size())
+
+        rec = torch.bmm(a, a_t)
+
+        #print('inner pro', rec.size())
+
+        org_adj = to_dense_adj(edge_index, batch)
+
+
+        #print('new' ,rec, 'org', org_adj)
+
+
+        '''#adj = torch.matmul(z, z.t())
+  
+  
+        #r_adj = to_dense_adj(recon_adj, batch)
+        #org_adj = to_dense_adj(edge_index, batch)
+  
+        #print(r_adj.size(), org_adj.size())
+  
+        recon_adj = self.edge_recon(z, edge_index)
+  
+  
+        pos_loss = -torch.log(
+            recon_adj + EPS).mean()
+  
+        # Do not include self-loops in negative samples
+        pos_edge_index, _ = remove_self_loops(edge_index)
+        pos_edge_index, _ = add_self_loops(pos_edge_index)
+  
+        neg_edge_index = negative_sampling(pos_edge_index, z.size(0)) #random thingggg
+        neg_loss = -torch.log(1 -
+                              self.edge_recon(z, neg_edge_index) +
+                              EPS).mean()
+  
+        return pos_loss + neg_loss'''
+
+        loss = F.binary_cross_entropy_with_logits(rec, org_adj)
+
+        return loss
+
+
+    def recon_loss1(self, z, edge_index, batch):
+
+        EPS = 1e-15
+        MAX_LOGSTD = 10
+        r"""Given latent variables :obj:`z`, computes the binary cross
+        entropy loss for positive edges :obj:`pos_edge_index` and negative
+        sampled edges.
+  
+        Args:
+            z (Tensor): The latent space :math:`\mathbf{Z}`.
+            pos_edge_index (LongTensor): The positive edges to train against.
+        """
+
+
+
+        recon_adj = self.edge_recon(z, edge_index)
+
+
+        pos_loss = -torch.log(
+            recon_adj + EPS).mean()
+
+        # Do not include self-loops in negative samples
+        pos_edge_index, _ = remove_self_loops(edge_index)
+        pos_edge_index, _ = add_self_loops(pos_edge_index)
+
+        neg_edge_index = negative_sampling(pos_edge_index, z.size(0)) #random thingggg
+        neg_loss = -torch.log(1 -
+                              self.edge_recon(z, neg_edge_index) +
+                              EPS).mean()
+
+        return pos_loss + neg_loss
+
+        #loss = F.binary_cross_entropy_with_logits(rec, org_adj)
+
+        #return loss
+
+    def get_embeddings(self, loader):
+
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        ret = []
+        y = []
+        with torch.no_grad():
+            for data in loader:
+
+                data.to(device)
+                x, edge_index, batch = data.x, data.edge_index, data.batch
+
+                #print(x, edge_index, data.x)
+                #x = torch.rand(data.batch.shape[0], 5).to(device)
+                x = torch.ones((batch.shape[0],5)).double().to(device)
+                #print('eval train', x.type())
+                __, _, class_mu, class_logvar = self.encoder(x, edge_index, batch)
+
+                grouped_mu, grouped_logvar = accumulate_group_evidence(
+                    class_mu.data, class_logvar.data, batch, True
+                )
+
+                accumulated_class_latent_embeddings = group_wise_reparameterize(
+                    training=False, mu=grouped_mu, logvar=grouped_logvar, labels_batch=batch, cuda=True
+                )
+
+                class_emb = global_mean_pool(accumulated_class_latent_embeddings, batch)
+
+                #print('clz emb ', class_emb[:5,:3])
+
+
+                ret.append(class_emb.cpu().numpy())
+                y.append(data.y.cpu().numpy())
+        ret = np.concatenate(ret, 0)
+        y = np.concatenate(y, 0)
+        return ret, y
+
+
 class GCNLayer(nn.Module):
     def __init__(self, in_ft, out_ft, bias=True):
         super(GCNLayer, self).__init__()
@@ -235,8 +496,6 @@ def train(dataset, gpu, num_layer=4, epoch=40, batch=64):
 
     adj, diff, feat, labels, num_nodes = load(dataset)
 
-    print(dataset, 'feat', feat)
-
     feat = torch.FloatTensor(feat).cuda()
     diff = torch.FloatTensor(diff).cuda()
     adj = torch.FloatTensor(adj).cuda()
@@ -331,8 +590,7 @@ if __name__ == '__main__':
     layers = [2, 8, 12]
     batch = [32, 64, 128, 256]
     epoch = [20, 40, 100]
-    #ds = ['MUTAG', 'PTC_MR', 'IMDB-BINARY', 'IMDB-MULTI', 'REDDIT-BINARY', 'REDDIT-MULTI-5K']
-    ds = ['IMDB-BINARY', 'IMDB-MULTI', 'REDDIT-BINARY', 'REDDIT-MULTI-5K']
+    ds = ['MUTAG', 'PTC_MR', 'IMDB-BINARY', 'IMDB-MULTI', 'REDDIT-BINARY', 'REDDIT-MULTI-5K']
     seeds = [123, 132, 321, 312, 231]
     for d in ds:
         print(f'####################{d}####################')
